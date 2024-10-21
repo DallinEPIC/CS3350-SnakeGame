a87e31c271ecbf7d082eab019f566d679f8c897a
Thomas Drown <72996582+tomatom92@users.noreply.github.com>
2024-10-21 12:46:48 -0600

    started tail behaviour
    started tail behaviour script and wrote comments for what a tail behaviour should be and have inside it.

--- Assets/Scripts/SnakeMovementBehaviour.cs
+++ Assets/Scripts/SnakeMovementBehaviour.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SnakeMovementBehaviour : MonoBehaviour
 {
+       
+    public List<Vector3> turnPositions = new();
     private Vector3 _movementDirection;
+    public Vector3 turnLocation;
     [SerializeField, Range(1,50)] private float _movementSpeed;
     void Start()
     {
@@ -14,11 +17,11 @@ public class SnakeMovementBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) { _movementDirection = Vector3.forward; }
+        if (Input.GetKeyDown(KeyCode.W)) { _movementDirection = Vector3.forward; turnPositions.Add(transform.position); }
         if (Input.GetKeyDown(KeyCode.S)) { _movementDirection = Vector3.back; }
         if (Input.GetKeyDown(KeyCode.A)) { _movementDirection = Vector3.left; }
         if (Input.GetKeyDown(KeyCode.D)) { _movementDirection = Vector3.right; }
 
-        transform.position += _movementDirection * _movementSpeed ;
+        transform.position += _movementDirection * _movementSpeed * Time.deltaTime;
     }
 }
